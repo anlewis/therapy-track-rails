3fc1ff15a584f4545e413dac29e20816187a2d5f
Anna Lewis <anna.noel.lewis@gmail.com>
2018-05-26 17:27:55 -0600

    Configure api setup

--- config/application.rb
+++ config/application.rb
@@ -18,6 +18,8 @@ Bundler.require(*Rails.groups)
 
 module TherapyTrackRails
   class Application < Rails::Application
+    # API setup
+    config.api_only = true
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
