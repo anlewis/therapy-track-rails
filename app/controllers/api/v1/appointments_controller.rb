bdfe5bf4c487cfd48e2e81c128fc55d34dff718f
Anna Lewis <anna.noel.lewis@gmail.com>
2018-05-26 17:34:09 -0600

    Add new appointment endpoint and make tests pass [#157897202]

--- app/controllers/api/v1/appointments_controller.rb
+++ app/controllers/api/v1/appointments_controller.rb
@@ -8,4 +8,19 @@ class Api::V1::AppointmentsController < ApplicationController
   rescue
     render status: 404
   end
+
+  def create
+    render json: Appointment.create(appointment_params)
+  end
+
+  private
+
+    def appointment_params
+      params.require(:appointment).permit(:summary,
+                                          :location,
+                                          :description,
+                                          :start_time,
+                                          :end_time
+                                         )
+    end
 end
