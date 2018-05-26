57ad624197085622aef049be6165b220a2235e31
Anna Lewis <anna.noel.lewis@gmail.com>
2018-05-26 17:20:44 -0600

    Make get single appointment by id endpoint pass tests [#157897202]

--- app/controllers/api/v1/appointments_controller.rb
+++ app/controllers/api/v1/appointments_controller.rb
@@ -2,4 +2,10 @@ class Api::V1::AppointmentsController < ApplicationController
   def index
     render json: Appointment.all
   end
+
+  def show
+    render json: Appointment.find(params[:id])
+  rescue
+    render status: 404
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :appointments, only: [:index]
+      resources :appointments, only: [:index, :show]
     end
   end
 end
