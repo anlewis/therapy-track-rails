b9bb8175af06905dae89a6171c2ff4268063a342
Anna Lewis <anna.noel.lewis@gmail.com>
2018-05-28 15:22:51 -0600

    Add user to seed file [#157920869]

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,15 @@
 require 'faker'
 
+user = User.create({
+  uid: 'MyId',
+  username: 'MyUsername',
+  provider: 'MyProvider',
+  name: 'MyName',
+  email: ENV['test_user_email'],
+  password: 'password',
+  password_confirmation: 'password'
+})
+
 5.times do |index|
   start_time = Faker::Time.forward(14, :evening)
   end_time = (start_time.to_time + 1.hours).to_datetime
@@ -8,6 +18,7 @@ require 'faker'
                       description: Faker::ChuckNorris.fact,
                       start_time: start_time,
                       end_time: end_time,
+                      user_id: user.id
                       )
 end
 
